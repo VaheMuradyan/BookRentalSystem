7c79cf18e5fb7b52184aaa174b325b4e49ee822e
VaheMuradyan <vahenmuradyan02@outlook.com>
2024-11-18 16:19:50 +0400

    add middleware

--- internal/handlers/user_handler.go
+++ internal/handlers/user_handler.go
@@ -3,9 +3,11 @@ package handlers
 import (
 	"github.com/VaheMuradyan/BookRentalSystem/db"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 	"net/http"
+	"time"
 )
 
 type UserHandler struct {
@@ -62,7 +64,45 @@ func (h *UserHandler) Signup(c *gin.Context) {
 }
 
 func (h *UserHandler) Login(c *gin.Context) {
+	var body struct {
+		Email    string
+		Password string
+	}
 
+	if err := c.BindJSON(&body); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Faild tou read body"})
+		return
+	}
+
+	var user db.User
+	h.db.First(&user, "email = ?", body.Email)
+
+	if user.ID == 0 {
+		c.IndentedJSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or password"})
+		return
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password)); err != nil {
+		c.IndentedJSON(http.StatusUnauthorized, gin.H{"error": "Invalid email or password"})
+		return
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
+
+	tokenString, err := token.SignedString([]byte("secret"))
+
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": "Invalid tou create token"})
+		return
+	}
+
+	c.SetSameSite(http.SameSiteLaxMode)
+	c.SetCookie("token", tokenString, 3600*24*30, "", "", false, true)
+
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "all done"})
 }
 
 func (h *UserHandler) PlaceOrder(c *gin.Context) {
